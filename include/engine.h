f38b7c644b8925f4c84d98c039d6a35e1ec014f0
Matt <68287092+NyerahMT@users.noreply.github.com>
2026-08-25 01:52:40 -0400

    Update engine.h

--- include/engine.h
+++ include/engine.h
@@ -20,136 +20,327 @@
 class Simulator;
 class Vehicle;
 class Transmission;
+
 class Engine : public Part {
-    public:
-        struct Parameters {
-            int cylinderBanks;
-            int cylinderCount;
-            int crankshaftCount;
-            int exhaustSystemCount;
-            int intakeCount;
+public:
+    struct Parameters {
+        int cylinderBanks = 0;
+        int cylinderCount = 0;
+        int crankshaftCount = 0;
+        int exhaustSystemCount = 0;
+        int intakeCount = 0;
 
-            std::string name;
+        std::string name;
 
-            double starterTorque = units::torque(90.0, units::ft_lb);
-            double starterSpeed = units::rpm(200);
-            double redline = units::rpm(6500);
-            double dynoMinSpeed = units::rpm(1000);
-            double dynoMaxSpeed = units::rpm(6500);
-            double dynoHoldStep = units::rpm(100);
+        double starterTorque =
+            units::torque(90.0, units::ft_lb);
 
-            Throttle *throttle;
+        double starterSpeed =
+            units::rpm(200);
 
-            double initialSimulationFrequency;
-            double initialHighFrequencyGain;
-            double initialNoise;
-            double initialJitter;
-        };
+        double redline =
+            units::rpm(6500);
 
-    public:
-        Engine();
-        virtual ~Engine();
+        double dynoMinSpeed =
+            units::rpm(1000);
 
-        void initialize(const Parameters &params);
-        virtual void destroy();
+        double dynoMaxSpeed =
+            units::rpm(6500);
 
-        std::string getName() const { return m_name; }
+        double dynoHoldStep =
+            units::rpm(100);
 
-        virtual Crankshaft *getOutputCrankshaft() const;
-        virtual void setSpeedControl(double s);
-        virtual double getSpeedControl();
-        virtual void setThrottle(double throttle);
-        virtual double getThrottle() const;
-        virtual double getThrottlePlateAngle() const;
-        virtual void calculateDisplacement();
-        double getDisplacement() const { return m_displacement; }
-        virtual double getIntakeFlowRate() const;
-        virtual void update(double dt);
+        Throttle *throttle = nullptr;
 
-        virtual double getManifoldPressure() const;
-        virtual double getIntakeAfr() const;
-        virtual double getExhaustO2() const;
-        virtual double getRpm() const;
-        virtual double getSpeed() const;
-        virtual bool isSpinningCw() const;
+        double initialSimulationFrequency =
+            10000.0;
 
-        virtual void resetFuelConsumption();
-        virtual double getTotalFuelMassConsumed() const;
-        double getTotalVolumeFuelConsumed() const;
+        double initialHighFrequencyGain =
+            0.01;
 
-        inline double getStarterTorque() const { return m_starterTorque; }
-        inline double getStarterSpeed() const { return m_starterSpeed; }
-        inline double getRedline() const { return m_redline; }
-        inline double getDynoMinSpeed() const { return m_dynoMinSpeed; }
-        inline double getDynoMaxSpeed() const { return m_dynoMaxSpeed; }
-        inline double getDynoHoldStep() const { return m_dynoHoldStep; }
+        double initialNoise =
+            1.0;
 
-        int getCylinderBankCount() const { return m_cylinderBankCount; }
-        int getCylinderCount() const { return m_cylinderCount; }
-        int getCrankshaftCount() const { return m_crankshaftCount; }
-        int getExhaustSystemCount() const { return m_exhaustSystemCount; }
-        int getIntakeCount() const { return m_intakeCount; }
-        int getMaxDepth() const;
+        double initialJitter =
+            0.5;
 
-        Crankshaft *getCrankshaft(int i) const { return &m_crankshafts[i]; }
-        CylinderBank *getCylinderBank(int i) const { return &m_cylinderBanks[i]; }
-        CylinderHead *getHead(int i) const { return &m_heads[i]; }
-        Piston *getPiston(int i) const { return &m_pistons[i]; }
-        ConnectingRod *getConnectingRod(int i) const { return &m_connectingRods[i]; }
-        IgnitionModule *getIgnitionModule() { return &m_ignitionModule; }
-        ExhaustSystem *getExhaustSystem(int i) const { return &m_exhaustSystems[i]; }
-        Intake *getIntake(int i) const { return &m_intakes[i]; }
-        CombustionChamber *getChamber(int i) const { return &m_combustionChambers[i]; }
-        Fuel *getFuel() { return &m_fuel; }
+        /*
+         * Community Edition simulation control.
+         *
+         * Older OES hardcoded this to 8 inside createSimulator().
+         * Keeping it on Engine::Parameters allows .mr files to select
+         * the intended fluid solver resolution.
+         */
+        int fluidSimulationSteps = 8;
+    };
 
-        double getSimulationFrequency() const { return m_initialSimulationFrequency; }
-        double getInitialHighFrequencyGain() const { return m_initialHighFrequencyGain; }
-        double getInitialNoise() const { return m_initialNoise; }
-        double getInitialJitter() const { return m_initialJitter; }
+public:
+    Engine();
+    virtual ~Engine();
 
-        virtual Simulator *createSimulator(
-            Vehicle *vehicle, Transmission *transmission, int outputAudioSampleRate = 44100);
+    void initialize(
+        const Parameters &params);
 
-    protected:
-        std::string m_name;
+    virtual void destroy();
 
-        Crankshaft *m_crankshafts;
-        int m_crankshaftCount;
+    std::string getName() const {
+        return m_name;
+    }
 
-        CylinderBank *m_cylinderBanks;
-        CylinderHead *m_heads;
-        int m_cylinderBankCount;
+    virtual Crankshaft *
+    getOutputCrankshaft() const;
 
-        Piston *m_pistons;
-        ConnectingRod *m_connectingRods;
-        CombustionChamber *m_combustionChambers;
-        int m_cylinderCount;
+    virtual void setSpeedControl(
+        double s);
 
-        double m_starterTorque;
-        double m_starterSpeed;
-        double m_redline;
-        double m_dynoMinSpeed;
-        double m_dynoMaxSpeed;
-        double m_dynoHoldStep;
+    virtual double
+    getSpeedControl();
 
-        double m_initialSimulationFrequency;
-        double m_initialHighFrequencyGain;
-        double m_initialNoise;
-        double m_initialJitter;
+    virtual void setThrottle(
+        double throttle);
 
-        ExhaustSystem *m_exhaustSystems;
-        int m_exhaustSystemCount;
+    virtual double
+    getThrottle() const;
 
-        Intake *m_intakes;
-        int m_intakeCount;
+    virtual double
+    getThrottlePlateAngle() const;
 
-        IgnitionModule m_ignitionModule;
-        Fuel m_fuel;
+    virtual void
+    calculateDisplacement();
 
-        Throttle *m_throttle;
+    double getDisplacement() const {
+        return m_displacement;
+    }
 
-        double m_throttleValue;
-        double m_displacement;
+    virtual double
+    getIntakeFlowRate() const;
+
+    virtual void update(
+        double dt);
+
+    virtual double
+    getManifoldPressure() const;
+
+    virtual double
+    getIntakeAfr() const;
+
+    virtual double
+    getExhaustO2() const;
+
+    virtual double
+    getRpm() const;
+
+    virtual double
+    getSpeed() const;
+
+    virtual bool
+    isSpinningCw() const;
+
+    virtual void
+    resetFuelConsumption();
+
+    virtual double
+    getTotalFuelMassConsumed() const;
+
+    double
+    getTotalVolumeFuelConsumed() const;
+
+    inline double
+    getStarterTorque() const
+    {
+        return m_starterTorque;
+    }
+
+    inline double
+    getStarterSpeed() const
+    {
+        return m_starterSpeed;
+    }
+
+    inline double
+    getRedline() const
+    {
+        return m_redline;
+    }
+
+    inline double
+    getDynoMinSpeed() const
+    {
+        return m_dynoMinSpeed;
+    }
+
+    inline double
+    getDynoMaxSpeed() const
+    {
+        return m_dynoMaxSpeed;
+    }
+
+    inline double
+    getDynoHoldStep() const
+    {
+        return m_dynoHoldStep;
+    }
+
+    inline int
+    getFluidSimulationSteps() const
+    {
+        return m_fluidSimulationSteps;
+    }
+
+    int getCylinderBankCount() const {
+        return m_cylinderBankCount;
+    }
+
+    int getCylinderCount() const {
+        return m_cylinderCount;
+    }
+
+    int getCrankshaftCount() const {
+        return m_crankshaftCount;
+    }
+
+    int getExhaustSystemCount() const {
+        return m_exhaustSystemCount;
+    }
+
+    int getIntakeCount() const {
+        return m_intakeCount;
+    }
+
+    int getMaxDepth() const;
+
+    Crankshaft *
+    getCrankshaft(int i) const
+    {
+        return &m_crankshafts[i];
+    }
+
+    CylinderBank *
+    getCylinderBank(int i) const
+    {
+        return &m_cylinderBanks[i];
+    }
+
+    CylinderHead *
+    getHead(int i) const
+    {
+        return &m_heads[i];
+    }
+
+    Piston *
+    getPiston(int i) const
+    {
+        return &m_pistons[i];
+    }
+
+    ConnectingRod *
+    getConnectingRod(int i) const
+    {
+        return &m_connectingRods[i];
+    }
+
+    IgnitionModule *
+    getIgnitionModule()
+    {
+        return &m_ignitionModule;
+    }
+
+    ExhaustSystem *
+    getExhaustSystem(int i) const
+    {
+        return &m_exhaustSystems[i];
+    }
+
+    Intake *
+    getIntake(int i) const
+    {
+        return &m_intakes[i];
+    }
+
+    CombustionChamber *
+    getChamber(int i) const
+    {
+        return &m_combustionChambers[i];
+    }
+
+    Fuel *
+    getFuel()
+    {
+        return &m_fuel;
+    }
+
+    double
+    getSimulationFrequency() const
+    {
+        return m_initialSimulationFrequency;
+    }
+
+    double
+    getInitialHighFrequencyGain() const
+    {
+        return m_initialHighFrequencyGain;
+    }
+
+    double
+    getInitialNoise() const
+    {
+        return m_initialNoise;
+    }
+
+    double
+    getInitialJitter() const
+    {
+        return m_initialJitter;
+    }
+
+    virtual Simulator *
+    createSimulator(
+        Vehicle *vehicle,
+        Transmission *transmission,
+        int outputAudioSampleRate = 44100);
+
+protected:
+    std::string m_name;
+
+    Crankshaft *m_crankshafts;
+    int m_crankshaftCount;
+
+    CylinderBank *m_cylinderBanks;
+    CylinderHead *m_heads;
+    int m_cylinderBankCount;
+
+    Piston *m_pistons;
+    ConnectingRod *m_connectingRods;
+    CombustionChamber *m_combustionChambers;
+    int m_cylinderCount;
+
+    double m_starterTorque;
+    double m_starterSpeed;
+    double m_redline;
+    double m_dynoMinSpeed;
+    double m_dynoMaxSpeed;
+    double m_dynoHoldStep;
+
+    double m_initialSimulationFrequency;
+    double m_initialHighFrequencyGain;
+    double m_initialNoise;
+    double m_initialJitter;
+
+    int m_fluidSimulationSteps;
+
+    ExhaustSystem *m_exhaustSystems;
+    int m_exhaustSystemCount;
+
+    Intake *m_intakes;
+    int m_intakeCount;
+
+    IgnitionModule m_ignitionModule;
+    Fuel m_fuel;
+
+    Throttle *m_throttle;
+
+    double m_throttleValue;
+    double m_displacement;
 };
 
 #endif /* ATG_ENGINE_SIM_ENGINE_H */
