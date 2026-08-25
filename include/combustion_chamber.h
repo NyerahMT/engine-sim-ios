0e28e88e7a42b300fd8c87c7e3827aabb42f9990
Matt <68287092+NyerahMT@users.noreply.github.com>
2026-08-25 02:02:28 -0400

    Update combustion_chamber.h

--- include/combustion_chamber.h
+++ include/combustion_chamber.h
@@ -10,114 +10,227 @@
 #include "fuel.h"
 
 class Engine;
-class CombustionChamber : public atg_scs::ForceGenerator {
-    public:
-        struct Parameters {
-            ::Piston *Piston;
-            CylinderHead *Head;
-            ::Fuel *Fuel;
-            Function *MeanPistonSpeedToTurbulence;
 
-            double StartingPressure;
-            double StartingTemperature;
-            double CrankcasePressure;
-        };
+class CombustionChamber
+    : public atg_scs::ForceGenerator
+{
+public:
+    struct Parameters {
+        ::Piston *Piston = nullptr;
+        CylinderHead *Head = nullptr;
+        ::Fuel *Fuel = nullptr;
 
-        struct FlameEvent {
-            double lit_n = 0;
-            double total_n = 0;
-            double percentageLit = 0;
-            double efficiency = 1.0;
-            double flameSpeed = 0.0;
+        Function *MeanPistonSpeedToTurbulence =
+            nullptr;
 
-            double lastVolume = 0.0;
-            double travel_x = 0.0;
-            double travel_y = 0.0;
-            GasSystem::Mix globalMix;
-        };
+        double StartingPressure = 0.0;
+        double StartingTemperature = 0.0;
+        double CrankcasePressure = 0.0;
 
-        struct FrictionModelParams {
-            double frictionCoeff = 0.06;
-            double breakawayFriction = units::force(50, units::N);
-            double breakawayFrictionVelocity = units::distance(0.1, units::m);
-            double viscousFrictionCoefficient = units::force(20, units::N);
-        };
+        /*
+         * Community Edition thermal controls.
+         *
+         * Old OES hardcoded:
+         *
+         *   block temperature = 90 C
+         *   heat transfer coefficient = 100
+         */
+        double BlockTemperature =
+            units::celcius(90.0);
 
-    public:
-        CombustionChamber();
-        virtual ~CombustionChamber();
+        double HeatTransferCoefficient =
+            100.0;
+    };
 
-        void initialize(const Parameters &params);
-        void destroy();
-        void setEngine(Engine *engine) { m_engine = engine; }
-        virtual void apply(atg_scs::SystemState *system);
+    struct FlameEvent {
+        double lit_n = 0;
+        double total_n = 0;
+        double percentageLit = 0;
+        double efficiency = 1.0;
+        double flameSpeed = 0.0;
 
-        CylinderHead *getCylinderHead() const { return m_head; }
-        Piston *getPiston() const { return m_piston; }
+        double lastVolume = 0.0;
+        double travel_x = 0.0;
+        double travel_y = 0.0;
 
-        double getFrictionForce() const;
-        double getVolume() const;
-        double pistonSpeed() const;
-        double calculateMeanPistonSpeed() const;
-        double calculateFiringPressure() const;
+        GasSystem::Mix globalMix;
+    };
 
-        bool isLit() const { return m_lit; }
-        bool popLitLastFrame();
+    struct FrictionModelParams {
+        double frictionCoeff =
+            0.06;
 
-        void ignite();
-        void update(double dt);
-        void flow(double dt);
+        double breakawayFriction =
+            units::force(
+                50,
+                units::N);
 
-        double lastEventAfr() const;
+        double breakawayFrictionVelocity =
+            units::distance(
+                0.1,
+                units::m);
 
-        double getLastIterationExhaustFlow() const { return m_exhaustFlow; }
+        double viscousFrictionCoefficient =
+            units::force(
+                20,
+                units::N);
+    };
 
-        void resetLastTimestepExhaustFlow() { m_lastTimestepTotalExhaustFlow = 0; }
-        double getLastTimestepExhaustFlow() const { return m_lastTimestepTotalExhaustFlow; }
+public:
+    CombustionChamber();
+    virtual ~CombustionChamber();
 
-        void resetLastTimestepIntakeFlow() { m_lastTimestepTotalIntakeFlow = 0; }
-        double getLastTimestepIntakeFlow() const { return m_lastTimestepTotalIntakeFlow; }
+    void initialize(
+        const Parameters &params);
 
-        Function *m_meanPistonSpeedToTurbulence;
-        GasSystem m_system;
-        GasSystem m_intakeRunnerAndManifold;
-        GasSystem m_exhaustRunnerAndPrimary;
-        FlameEvent m_flameEvent;
-        bool m_lit;
+    void destroy();
 
-        FrictionModelParams m_frictionModel;
+    void setEngine(
+        Engine *engine)
+    {
+        m_engine =
+            engine;
+    }
 
-        double m_peakTemperature;
-        double m_nBurntFuel;
+    virtual void apply(
+        atg_scs::SystemState *system);
 
-    protected:
-        double calculateFrictionForce(double v) const;
-        void updateCycleStates();
+    CylinderHead *
+    getCylinderHead() const
+    {
+        return m_head;
+    }
 
-        double m_intakeFlowRate;
-        double m_exhaustFlowRate;
+    Piston *
+    getPiston() const
+    {
+        return m_piston;
+    }
 
-        double m_manifoldToRunnerFlowRate;
-        double m_primaryToCollectorFlowRate;
-        double m_cylinderCrossSectionSurfaceArea;
-        double m_cylinderWidthApproximation;
+    double
+    getFrictionForce() const;
 
-        double m_lastTimestepTotalExhaustFlow;
-        double m_lastTimestepTotalIntakeFlow;
-        double m_exhaustFlow;
+    double
+    getVolume() const;
 
-        double m_crankcasePressure;
+    double
+    pistonSpeed() const;
 
-        double *m_pressure;
-        double *m_pistonSpeed;
-        static constexpr int StateSamples = 256;
+    double
+    calculateMeanPistonSpeed() const;
 
-        bool m_litLastFrame;
+    double
+    calculateFiringPressure() const;
 
-        Piston *m_piston;
-        CylinderHead *m_head;
-        Engine *m_engine;
-        Fuel *m_fuel;
+    bool isLit() const {
+        return m_lit;
+    }
+
+    bool
+    popLitLastFrame();
+
+    void ignite();
+
+    void update(
+        double dt);
+
+    void flow(
+        double dt);
+
+    double
+    lastEventAfr() const;
+
+    double
+    getLastIterationExhaustFlow() const
+    {
+        return m_exhaustFlow;
+    }
+
+    void
+    resetLastTimestepExhaustFlow()
+    {
+        m_lastTimestepTotalExhaustFlow =
+            0;
+    }
+
+    double
+    getLastTimestepExhaustFlow() const
+    {
+        return
+            m_lastTimestepTotalExhaustFlow;
+    }
+
+    void
+    resetLastTimestepIntakeFlow()
+    {
+        m_lastTimestepTotalIntakeFlow =
+            0;
+    }
+
+    double
+    getLastTimestepIntakeFlow() const
+    {
+        return
+            m_lastTimestepTotalIntakeFlow;
+    }
+
+    Function *
+        m_meanPistonSpeedToTurbulence;
+
+    GasSystem m_system;
+    GasSystem m_intakeRunnerAndManifold;
+    GasSystem m_exhaustRunnerAndPrimary;
+
+    FlameEvent m_flameEvent;
+
+    bool m_lit;
+
+    FrictionModelParams
+        m_frictionModel;
+
+    double m_peakTemperature;
+    double m_nBurntFuel;
+
+protected:
+    double calculateFrictionForce(
+        double v) const;
+
+    void updateCycleStates();
+
+    double m_intakeFlowRate;
+    double m_exhaustFlowRate;
+
+    double m_manifoldToRunnerFlowRate;
+    double m_primaryToCollectorFlowRate;
+
+    double m_cylinderCrossSectionSurfaceArea;
+    double m_cylinderWidthApproximation;
+
+    double m_lastTimestepTotalExhaustFlow;
+    double m_lastTimestepTotalIntakeFlow;
+
+    double m_exhaustFlow;
+
+    double m_crankcasePressure;
+
+    /*
+     * CE thermal state.
+     */
+    double m_blockTemperature;
+    double m_heatTransferCoefficient;
+
+    double *m_pressure;
+    double *m_pistonSpeed;
+
+    static constexpr int StateSamples =
+        256;
+
+    bool m_litLastFrame;
+
+    Piston *m_piston;
+    CylinderHead *m_head;
+    Engine *m_engine;
+    Fuel *m_fuel;
 };
 
-#endif /* ATG_ENGINE_SIM_COMBUSTION_CHAMBER_H */
+#endif
