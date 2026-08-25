3422191b11a5a90bd3f1f2d21f33344fd194f1ff
Matt <68287092+NyerahMT@users.noreply.github.com>
2026-08-25 01:32:08 -0400

    Update transmission.h

--- include/transmission.h
+++ include/transmission.h
@@ -6,40 +6,115 @@
 #include "scs.h"
 
 class Transmission {
-    public:
-        struct Parameters {
-            int GearCount;
-            const double *GearRatios;
-            double MaxClutchTorque;
-        };
+public:
+    struct Parameters {
+        int GearCount = 0;
 
-    public:
-        Transmission();
-        ~Transmission();
+        const double *GearRatios =
+            nullptr;
 
-        void initialize(const Parameters &params);
-        void update(double dt);
-        void addToSystem(
-            atg_scs::RigidBodySystem *system,
-            atg_scs::RigidBody *rotatingMass,
-            Vehicle *vehicle,
-            Engine *engine);
-        void changeGear(int newGear);
-        inline int getGear() const { return m_gear; }
-        inline void setClutchPressure(double pressure) { m_clutchPressure = pressure; }
-        inline double getClutchPressure() const { return m_clutchPressure; }
+        double MaxClutchTorque =
+            0.0;
 
-    protected:
-        atg_scs::ClutchConstraint m_clutchConstraint;
-        atg_scs::RigidBody *m_rotatingMass;
-        Vehicle *m_vehicle;
+        /*
+         * Community Edition clutch dynamics.
+         */
+        double MaxClutchFlex =
+            0.0;
 
-        int m_gear;
-        int m_newGear;
-        int m_gearCount;
-        double *m_gearRatios;
-        double m_maxClutchTorque;
-        double m_clutchPressure;
+        bool LimitClutchFlex =
+            false;
+
+        double ClutchStiffness =
+            0.0;
+
+        double ClutchDamping =
+            0.0;
+    };
+
+public:
+    Transmission();
+    ~Transmission();
+
+    void initialize(
+        const Parameters &params);
+
+    void update(double dt);
+
+    void addToSystem(
+        atg_scs::RigidBodySystem *system,
+        atg_scs::RigidBody *rotatingMass,
+        Vehicle *vehicle,
+        Engine *engine);
+
+    void changeGear(
+        int newGear);
+
+    inline int getGear() const {
+        return m_gear;
+    }
+
+    inline void setClutchPressure(
+        double pressure)
+    {
+        if (pressure < 0.0) {
+            pressure = 0.0;
+        }
+
+        if (pressure > 1.0) {
+            pressure = 1.0;
+        }
+
+        m_clutchPressure =
+            pressure;
+    }
+
+    inline double
+    getClutchPressure() const
+    {
+        return m_clutchPressure;
+    }
+
+    inline double
+    getMaxClutchFlex() const
+    {
+        return m_maxClutchFlex;
+    }
+
+    inline double
+    getClutchStiffness() const
+    {
+        return m_clutchStiffness;
+    }
+
+    inline double
+    getClutchDamping() const
+    {
+        return m_clutchDamping;
+    }
+
+protected:
+    atg_scs::ClutchConstraint
+        m_clutchConstraint;
+
+    atg_scs::RigidBody
+        *m_rotatingMass;
+
+    Vehicle *m_vehicle;
+
+    int m_gear;
+    int m_newGear;
+    int m_gearCount;
+
+    double *m_gearRatios;
+
+    double m_maxClutchTorque;
+    double m_clutchPressure;
+
+    double m_maxClutchFlex;
+    bool m_limitClutchFlex;
+    double m_clutchStiffness;
+    double m_clutchDamping;
 };
 
-#endif /* ATG_ENGINE_SIM_TRANSMISSION_H */
+#endif
